53ef1cb37d6df8b99c6b87c22f44a423f2de81a5
april067 <ik.kiev.ua@gmail.com>
2023-12-17 23:53:52 +0200

    src

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+import * as gallery from './js/01-gallery';
+import * as video from './js/02-video';
+import * as feedback from './js/03-feedback';
+
 console.log('Hello world!');
---
+++ src/js/01-gallery.js
@@ -0,0 +1,4 @@
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+// console.log(galleryItems);

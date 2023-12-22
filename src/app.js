2da1425926e5b2648bc2ea5812bb042497b84cc0
april067 <ik.kiev.ua@gmail.com>
2023-12-22 15:30:38 +0200

    first task

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import * as gallery from './js/01-gallery';
-import * as video from './js/02-video';
-import * as feedback from './js/03-feedback';
-
-console.log('Hello world!');
--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,30 @@
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 
-// console.log(galleryItems);
+export const galleryRef = document.querySelector('.gallery');
+
+export const markup = createMarkup(galleryItems);
+
+function createMarkup(data) {
+	return data
+		.map(({ preview, original, description }) => {
+			return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"  />
+   </a>
+</li>
+    `;
+		})
+		.join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	nav: false,
+	captionsData: 'alt',
+	captionDelay: 250,
+	close: false,
+});
